3b9c9f19537a80c15a45d6a40a70a833d3d6f347
Firaol Bulo <137378704+firaolkiya@users.noreply.github.com>
2024-03-05 21:24:25 +0300

    Add solution for N-Queens II

---
+++ leetcode/n-queens-ii.py
@@ -0,0 +1,34 @@
+class Solution:
+    def totalNQueens(self, n: int) -> List[List[str]]:
+        posDiagonal = set()
+        negDiagonal = set()
+        column = set()
+        ans=0
+        chessBoard = [["."]*n for i in range(n)]
+        isCheckmate = lambda row, col: (row + col) in posDiagonal or (row - col) in negDiagonal or col in column
+        def validateBoard(row):
+            nonlocal ans
+            if row==n:
+                row_content=["".join(r_th) for r_th in chessBoard]
+                ans+=1
+                return
+            
+            for row_col in range(n):
+                if isCheckmate(row,row_col):
+                    continue
+                column.add(row_col)
+                negDiagonal.add(row-row_col)
+                posDiagonal.add(row+row_col)
+                chessBoard[row][row_col]="Q"
+
+                validateBoard(row+1)
+                column.remove(row_col)
+                negDiagonal.remove(row-row_col)
+                posDiagonal.remove(row+row_col)
+                chessBoard[row][row_col]="."
+        validateBoard(0)
+        return ans
+
+
+
+
